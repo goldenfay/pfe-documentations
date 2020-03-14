5dc9636a17a592819eafb3d2bea2fb1c2fe1dafc
goldenfay <selmanefaycal@hotmail.fr>
2020-03-14 12:03:27 +0100

    push

--- ML_package/data_loaders/loaders.py
+++ ML_package/data_loaders/loaders.py
@@ -84,8 +84,8 @@ class SimpleLoader(Loader):
 
 class GenericLoader(Loader):
 
-    def __init__(self,img_gt_dmap_list,reset_samplers=True):
-        super(SimpleLoader,self).__init__(reset_samplers)
+    def __init__(self,img_gt_dmap_list,reset_samplers=False):
+        super(GenericLoader,self).__init__(reset_samplers)
         self.img_gt_dmap_list=img_gt_dmap_list
         
         
