3a55ab4941570d4a384376a4bfd819d0fed2a8ee
goldenfay <selmanefaycal@hotmail.fr>
2020-04-02 18:16:18 +0100

    push

--- ML_package/models/model.py
+++ ML_package/models/model.py
@@ -86,7 +86,7 @@ class Model(NN.Module):
                 loss.backward()
                 self.optimizer.step()
             #print("epoch:",epoch,"loss:",epoch_loss/len(dataloader))
-            print(self.parameters()[0])
+            print(self[0].weight)
                 # Log results in checkpoints directory
             epochs_list.append(epoch)
             train_loss_list.append(epoch_loss/len(train_dataloader))
