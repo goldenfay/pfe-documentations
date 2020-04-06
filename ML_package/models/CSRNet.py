56947ebebaf28bb0e2ed2ff4a0603bab1cf53f50
goldenfay <selmanefaycal@hotmail.fr>
2020-04-06 14:46:46 +0100

    push

--- ML_package/models/CSRNet.py
+++ ML_package/models/CSRNet.py
@@ -26,8 +26,7 @@ class CSRNet(Model):
         if not weightsFlag:
             mod = models.vgg16(pretrained = True)
             self._initialize_weights()
-            for i in range(len(self.frontEnd.state_dict().items())):
-                self.frontEnd.state_dict().items()[i][1].data[:] = mod.state_dict().items()[i][1].data[:]
+            self.frontEnd.state_dict= mod.state_dict()
         
     
     
@@ -38,8 +37,7 @@ class CSRNet(Model):
         if not weightsFlag:
             mod = models.vgg16(pretrained = True)
             self._initialize_weights()
-            for i in range(len(self.frontEnd.state_dict().items())):
-                self.frontEnd.state_dict().items()[i][1].data[:] = mod.state_dict().items()[i][1].data[:]
+            self.frontEnd.state_dict= mod.state_dict()
 
     def forward(self,x):
         x = self.frontEnd(x)
