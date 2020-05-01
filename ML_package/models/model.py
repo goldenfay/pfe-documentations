11a036715ac540356af3c366431db59ac1668ca5
goldenfay <selmanefaycal@hotmail.fr>
2020-05-01 18:47:18 +0100

    fixing issues

--- ML_package/models/model.py
+++ ML_package/models/model.py
@@ -292,7 +292,7 @@ class Model(NN.Module):
         try:
             torch.save(model, path)
         except:
-            torch.save(model.module, path)   
+            torch.save(model.cpu(), path)   
 
     def make_summary(self, finished=False, test_mse=None, test_mae=None):
         path = os.path.join(self.checkpoints_dir, 'summary.json')
