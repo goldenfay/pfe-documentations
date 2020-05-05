8d6565772dfe85a29836a21f6c174d1a93445eab
goldenfay <selmanefaycal@hotmail.fr>
2020-05-05 10:45:12 +0100

    fixing bug

--- ML_package/bases/params.py
+++ ML_package/bases/params.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 class TrainParams:
     
-    def __init__(self,device,module:nn.Module,lr,momentum,maxEpochs,criterionMethode,optimizationMethod):
+    def __init__(self,device,module:nn.Module,lr,momentum,maxEpochs,criterionMethode,optimizationMethod,**kargs):
         super().__init__()
         self.device=device
         self.lr=lr
@@ -26,15 +26,13 @@ class TrainParams:
             self.optimizer=torch.optim.SGD(module.parameters(), lr=self.lr,
                                 momentum=self.momentum)
         elif optimizationMethod.lower()=="Adam".lower():
-            self.optimizer=torch.optim.Adam(module.parameters(), lr=self.lr,
-                                momentum=self.momentum)
+            self.optimizer=torch.optim.Adam(module.parameters(), lr=self.lr)
             
         elif optimizationMethod.lower()=="RMSprop".lower():
             self.optimizer=torch.optim.RMSprop(module.parameters(), lr=self.lr,
                                 momentum=self.momentum)
         elif optimizationMethod.lower()=="LBFGS".lower():
-            self.optimizer=torch.optim.LBFGS(module.parameters(), lr=self.lr,
-                                momentum=self.momentum)
+            self.optimizer=torch.optim.LBFGS(module.parameters(), lr=self.lr)
         else: raise TypeError("Invalide argument\n\t 'Criterion' Value is invalide ")
         
 
