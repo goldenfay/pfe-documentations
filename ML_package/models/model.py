661b41961bb6f16cd958d3da74495bb04c8b2952
goldenfay <selmanefaycal@hotmail.fr>
2020-05-21 23:31:04 +0100

    new push

--- ML_package/models/model.py
+++ ML_package/models/model.py
@@ -132,7 +132,8 @@ class Model(NN.Module):
                         gt_dmap.size()[2], gt_dmap.size()[3]), mode='bilinear')
                     # est_dmap = F.interpolate(est_dmap, size=(
                     #     gt_dmap.size()[1], gt_dmap.size()[2]), mode='bilinear')
-
+                if torch.isnan(est_dmap): print('Estimated is nan')
+                if torch.isnan(gt_dmap): print('Ground truth is nan')
                     # calculate loss
                 loss = train_params.criterion(est_dmap, gt_dmap)
                 epoch_loss += loss.item()
