f902bb83d96b0cdff3b7a0bb88edaeff69241977
goldenfay <selmanefaycal@hotmail.fr>
2020-05-23 23:07:18 +0100

    New dataloaders changes

--- ML_package/process/main.py
+++ ML_package/process/main.py
@@ -291,7 +291,7 @@ if __name__=="__main__":
     else :
         root = 'C:\\Users\\PC\\Desktop\\PFE related\\existing works\\Zhang_Single-Image_Crowd_Counting_CVPR_2016_paper code sample\\MCNN-pytorch-master\\MCNN-pytorch-master\\ShanghaiTech'
     dm_generator_type="knn_gaussian_kernal"
-    dataset_names=["ShanghaiTech_partA","ShanghaiTech_partB","NWPU"]
+    dataset_names=["ShanghaiTech_partA","ShanghaiTech_partB"]
     dm_generator=None
     loader_type="GenericLoader"
     model_type=args['model_type']#sys.argv[2] if len(sys.argv)>2 else "CSRNet"
