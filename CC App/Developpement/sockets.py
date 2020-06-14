427ecbbf444fde4e5d12bcbf3fac3978ad0db996
goldenfay <selmanefaycal@hotmail.fr>
2020-06-14 22:23:46 +0100

    Fixing Server side issues

---
+++ CC App/Developpement/sockets.py
@@ -0,0 +1,38 @@
+import socketio
+
+
+class ClientSocket(socketio.Client):
+
+    def __init__(self, **kwargs):
+        super(ClientSocket,self).__init__(**kwargs)
+        self.init_handlers()
+
+    def init_handlers(self):
+        @self.event
+        def connect():
+            print("[INFO] Connected to server successfully.")
+
+        @self.event
+        def connect_error():
+            print("[Connection Error] Couldn't connect to Server")
+
+        @self.event
+        def disconnect():
+            print("[INFO] Socket disconnected.")
+
+    def add_handlers(self,handlers):
+        for event,event_handler in handlers:
+            self.on(event,handler=event_handler)   
+            
+
+        # @self.on('server-error')
+        # def disconnect():
+        #     print("[INFO] Socket disconnected.")    
+        # @self.on('process-done')
+        # def disconnect():
+        #     print("[INFO] Socket disconnected.")    
+        # @self.on('send-image')
+        # def disconnect():
+        #     print("[INFO] Socket disconnected.")    
+
+            
