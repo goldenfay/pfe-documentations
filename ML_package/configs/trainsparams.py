fdb973971ddf408101233260e746e6ac22887d3d
goldenfay <selmanefaycal@hotmail.fr>
2020-08-06 10:17:29 +0100

    [Feature] Add image transformations for data augmentations

--- ML_package/configs/trainsparams.py
+++ ML_package/configs/trainsparams.py
@@ -3,26 +3,26 @@ CSRNet_PARAMS={"lr":1e-6,
             "maxEpochs":1000,
             "criterionMethode":'L2Loss',
             "optimizationMethod":'SGD',
-            'batch_size': 1
+            'batch_size': 3
             }
 SANet_PARAMS={"lr":1e-5,
             "momentum":0.95,
             "maxEpochs":1000,
             "criterionMethode":'L2Loss',
             "optimizationMethod":'Adam',
-            'batch_size': 1
+            'batch_size': 3
             }
 MCNN_PARAMS={"lr":1e-6,
             "momentum":0.95,
             "maxEpochs":1000,
             "criterionMethode":'MSELoss',
             "optimizationMethod":'SGD',
-            'batch_size': 1
+            'batch_size': 3
             }
 CCNN_PARAMS={"lr":1e-6,
             "momentum":0.95,
             "maxEpochs":1000,
             "criterionMethode":'MSELoss',
             "optimizationMethod":'Adam',
-            'batch_size': 1
+            'batch_size': 3
             }
